3a950e5321e0001a9a426f84cbaa2779e711b846
user <user@DESKTOP-VEPMOV7>
2021-09-01 00:23:22 +0900

    start_01

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello java");
+
+	}
+
+}
+//syso ctrl+space 단축기
+//bin에 main클래스 파일 들어있음 기계에서 읽을 수 있도록 컴파일 내용 저장
